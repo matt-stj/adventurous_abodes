b68100ca60e963be2d3af5e5fbd2851b26e5d34f
Michael Wong <michael.brian.wong@gmail.com>
2015-12-03 16:06:59 -0700

    adds test for editing user with bad credentials

--- test/integration/user_dashboard_test.rb
+++ test/integration/user_dashboard_test.rb
@@ -20,6 +20,17 @@ class UserDashboardTest < ActionDispatch::IntegrationTest
     assert page.has_content?("aaron")
   end
 
+  test "user can edit account but must have name" do
+    create_and_login_user
+    assert page.has_content?("Welcome")
+    click_link "Edit Account"
+    fill_in "Username", with: ""
+    fill_in "Password", with: "pass"
+    click_button "Update Account"
+
+    assert page.has_content?("Username can't be blank")
+  end
+
   test "user can delete their account" do
     create_and_login_user
     assert page.has_content?("Welcome, Nicole")
