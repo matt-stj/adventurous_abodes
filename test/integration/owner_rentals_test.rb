4f2cce4927e7ad9ac854a1135c052d3b3b12e429
Michael Wong <michael.brian.wong@gmail.com>
2015-12-03 14:00:35 -0700

    adds testing to ensure an owner enters a name when editing or creating a rental

--- test/integration/owner_rentals_test.rb
+++ test/integration/owner_rentals_test.rb
@@ -3,12 +3,9 @@ require "test_helper"
 class OwnerRentalsTest < ActionDispatch::IntegrationTest
   test "owner can create a rental" do
     login_owner
-
     click_link("Add Rental")
-
     assert_equal new_owners_rental_path, current_path
     assert page.has_content?("Add a New Rental")
-
     fill_in "Name", with: "Brick Castle"
     fill_in "Description", with: "Have a ball sleeping in the alps!"
     fill_in "Price", with: 1000
@@ -20,15 +17,31 @@ class OwnerRentalsTest < ActionDispatch::IntegrationTest
     assert page.has_content?("Brick Castle")
   end
 
+  test "owner can create a rental and must have name" do
+    login_owner
+    click_link("Add Rental")
+    assert_equal new_owners_rental_path, current_path
+    assert page.has_content?("Add a New Rental")
+    fill_in "Name", with: ""
+    fill_in "Description", with: "Have a ball sleeping in the alps!"
+    fill_in "Price", with: 1000
+    fill_in "Rental type", with: "Castle"
+    click_button "Create Rental"
+
+    assert_equal new_owners_rental_path, current_path
+    refute page.has_content?("The rental 'Brick Castle' has been created")
+    assert page.has_content?("Name can't be blank")
+  end
+
   test "owner can view all rentals" do
-    skip
     create_rentals(2, "Castle")
     login_owner
     click_link ("View All Rentals")
 
     assert "/owners/rentals", current_path
     assert page.has_content?("Rentals")
-    assert page.has_content?("Castle")
+    assert page.has_content?("Castle 1")
+    assert page.has_content?("Castle 2")
     assert page.has_content?("ACTIVE")
     assert page.has_button?("Edit")
   end
@@ -37,9 +50,6 @@ class OwnerRentalsTest < ActionDispatch::IntegrationTest
     create_rentals(1, "Castle")
     login_owner
     click_link ("View All Rentals")
-
-    assert owners_rentals_path, current_path
-    assert page.has_content?("Castle")
     click_button "Edit"
     assert "/owners/rentals/#{Rental.first.id}/edit", current_path
 
@@ -51,6 +61,20 @@ class OwnerRentalsTest < ActionDispatch::IntegrationTest
     assert owners_dashboard_path, current_path
     assert page.has_content?("Neuschwanstein")
     refute page.has_content?("Castle 1")
+  end
+
+  test "owner can edit an existing rental and must have name" do
+    create_rentals(1, "Castle")
+    login_owner
+    click_link ("View All Rentals")
+    click_button "Edit"
+    fill_in "Name",   with: ""
+    fill_in "Price",  with: 650
+    fill_in "Status", with: "Active"
+    click_button "Update Rental"
+
+    assert new_owners_rental_path, current_path
+    assert page.has_content?("Name can't be blank")
   end
 
   test "owner can delete an existing rental" do
