502dc25ec8ceb1304376f36d064dba3cd19b0954
ShannonPaige <spaige@gmail.com>
2015-12-07 13:36:39 -0700

    Finalize javascript dropdown with all rental types

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -18,6 +18,42 @@ $(document).ready(function() {
                 $(".rentals").not(".shack").hide();
                 $(".shack").show();
             }
+            else if($(this).attr("value")=="Treehouse"){
+                $(".rentals").not(".treehouse").hide();
+                $(".treehouse").show();
+            }
+            else if($(this).attr("value")=="Penthouse"){
+                $(".rentals").not(".penthouse").hide();
+                $(".penthouse").show();
+            }
+            else if($(this).attr("value")=="Spaceship"){
+                $(".rentals").not(".spaceship").hide();
+                $(".spaceship").show();
+            }
+            else if($(this).attr("value")=="Submarine"){
+                $(".rentals").not(".submarine").hide();
+                $(".submarine").show();
+            }
+            else if($(this).attr("value")=="Mansion"){
+                $(".rentals").not(".mansion").hide();
+                $(".mansion").show();
+            }
+            else if($(this).attr("value")=="Capsule"){
+                $(".rentals").not(".capsule").hide();
+                $(".capsule").show();
+            }
+            else if($(this).attr("value")=="Igloo"){
+                $(".rentals").not(".igloo").hide();
+                $(".igloo").show();
+            }
+            else if($(this).attr("value")=="Attic"){
+                $(".rentals").not(".attic").hide();
+                $(".attic").show();
+            }
+            else if($(this).attr("value")=="Storage Container"){
+                $(".rentals").not(".storageContainer").hide();
+                $(".storageContainer").show();
+            }
             else{
                 $(".rentals").hide();
             }
