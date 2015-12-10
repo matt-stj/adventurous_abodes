6d95b14ab0e7d302ff2451e0d1552056d852bcc8
ShannonPaige <spaige@gmail.com>
2015-12-10 09:48:09 -0700

    Delete extranious method in Rentals Helper

--- app/helpers/rentals_helper.rb
+++ app/helpers/rentals_helper.rb
@@ -4,12 +4,4 @@ module RentalsHelper
         link_to "Edit Rental", edit_owners_rental_path(@rental.id)
       end
   end
-
-  def get_purchase_link_or_retired_notice
-    if @rental.retired?
-      content_tag :p, "This rental has been retired and may no longer be purchased."
-    else
-      link_to "Reserve it!", new_cart_rental_path(id: @rental.id)
-    end
-  end
 end
